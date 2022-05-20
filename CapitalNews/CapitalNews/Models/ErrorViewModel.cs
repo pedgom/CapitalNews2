1fea36123e824d48425f38653676b554dfb9bea0
Pedro Gomes <aluno21279@ipt.pt>
2022-05-20 12:15:21 +0100

    Primeiro Commit

---
+++ CapitalNews/CapitalNews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CapitalNews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
